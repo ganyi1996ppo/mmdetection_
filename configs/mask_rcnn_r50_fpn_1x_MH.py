11f4f0614ce18d0c5ba4908afb13b01d6048ca6c
Yi G <627780002ganyi@sjtu.edu.cn>
2019-12-23 21:43:25 +0800

    bf

--- configs/mask_rcnn_r50_fpn_1x_MH.py
+++ configs/mask_rcnn_r50_fpn_1x_MH.py
@@ -151,7 +151,7 @@ test_pipeline = [
         ])
 ]
 data = dict(
-    imgs_per_gpu=1,
+    imgs_per_gpu=2,
     workers_per_gpu=2,
     train=dict(
         type=dataset_type,
@@ -169,7 +169,7 @@ data = dict(
         img_prefix=data_root + 'val2017/',
         pipeline=test_pipeline))
 # optimizer
-optimizer = dict(type='SGD', lr=0.0025, momentum=0.9, weight_decay=0.0001)
+optimizer = dict(type='SGD', lr=0.005, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
 # learning policy
 lr_config = dict(
