517c89911abb8ee95cc41712180eef8dc93a2694
Yi G <627780002ganyi@sjtu.edu.cn>
2020-01-19 09:47:39 +0800

    a

--- configs/mask_protohint2_rcnn_r50_fpn_1x.py
+++ configs/mask_protohint2_rcnn_r50_fpn_1x.py
@@ -224,7 +224,7 @@ evaluation = dict(interval=1)
 total_epochs = 12
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-work_dir = './work_dirs/proto_seg_mask_rcnn_r50_fpn_1x_seg'
+work_dir = './work_dirs/proto_2seg_mask_rcnn_r50_fpn_1x_seg'
 load_from = None
 resume_from = None
 workflow = [('train', 1)]
