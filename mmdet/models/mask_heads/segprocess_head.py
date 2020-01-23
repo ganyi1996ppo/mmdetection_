b9bc9a51eae2ae516ccd8d45c52e7905bcee594b
Yi G <627780002ganyi@sjtu.edu.cn>
2020-01-23 09:39:23 +0800

    a

--- mmdet/models/mask_heads/segprocess_head.py
+++ mmdet/models/mask_heads/segprocess_head.py
@@ -13,7 +13,8 @@ class SemanticProcessHead(nn.Module):
     def __init__(self,
                  num_convs=4,
                  in_channels=80,
-                 conv_out_channels=80,
+                 conv_out_channels=256,
+                 groups=True,
                  conv_cfg=None,
                  norm_cfg=None):
         super(SemanticProcessHead, self).__init__()
@@ -23,24 +24,42 @@ class SemanticProcessHead(nn.Module):
         self.conv_cfg = conv_cfg
         self.norm_cfg = norm_cfg
         self.fp16_enabled = False
+        self.groups = groups
 
-        self.lateral_conv = ConvModule(self.in_channels,self.conv_out_channels, 3, stride=2, padding=1,
-                                       groups=self.in_channels, conv_cfg=self.conv_cfg,
-                                       norm_cfg=self.norm_cfg)
+        if self.groups:
+            self.lateral_conv = ConvModule(self.in_channels,self.conv_out_channels, 3, stride=2, padding=1,
+                                           groups = self.in_channels,
+                                           conv_cfg=self.conv_cfg,
+                                           norm_cfg=self.norm_cfg)
+        else:
+            self.lateral_conv = ConvModule(
+                self.in_channels, self.conv_out_channels, 3, stride=2, padding=1,
+                conv_cfg=self.conv_cfg,
+                norm_cfg=self.norm_cfg
+            )
 
         self.convs = nn.ModuleList()
         for i in range(self.num_convs):
             in_channels = self.in_channels if i == 0 else conv_out_channels
-            self.convs.append(
-                ConvModule(
-                    in_channels,
-                    in_channels,
-                    3,
-                    dilation=2,
-                    padding=2,
-                    groups=in_channels,
-                    conv_cfg=self.conv_cfg,
-                    norm_cfg=self.norm_cfg))
+            if self.groups:
+                self.convs.append(
+                    ConvModule(
+                        in_channels,
+                        conv_out_channels,
+                        3,
+                        padding=1,
+                        groups=in_channels,
+                        conv_cfg=self.conv_cfg,
+                        norm_cfg=self.norm_cfg))
+            else:
+                self.convs.append(
+                    ConvModule(
+                        in_channels,
+                        conv_out_channels,
+                        3,
+                        padding=1,
+                        conv_cfg=self.conv_cfg,
+                        norm_cfg=self.norm_cfg))
 
         # self.conv_embedding = ConvModule(
         #     conv_out_channels,
