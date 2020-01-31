2c0904de2146efba6467947dec05356d6686dcd3
Yi G <627780002ganyi@sjtu.edu.cn>
2020-01-31 18:23:25 +0800

    modify spn, more flexible

--- mmdet/models/necks/SPN.py
+++ mmdet/models/necks/SPN.py
@@ -14,6 +14,8 @@ class SemanticPyramidNeck(nn.Module):
     def __init__(self,
                  feature_channels=256,
                  mask_channels=80,
+                 num_convs=3,
+                 ds_method='interpolate',
                  num_levels = 5,
                  proto_out=8,
                  conv_out_channels=256,
@@ -22,6 +24,8 @@ class SemanticPyramidNeck(nn.Module):
                  norm_cfg=None):
         super(SemanticPyramidNeck, self).__init__()
         self.mask_channels = mask_channels
+        self.num_convs = num_convs
+        self.ds_method = ds_method
         self.num_levels = num_levels
         self.conv_out_channels = conv_out_channels
         self.feature_channels = feature_channels
@@ -30,11 +34,19 @@ class SemanticPyramidNeck(nn.Module):
         self.proto_out = proto_out
         self.fp16_enabled = False
         self.groups = groups
+        if self.ds_method == 'conv':
+            self.ds_convs = nn.ModuleList()
+            for i in range(self.num_levels-1):
+                self.ds_convs.append(
+                    ConvModule(self.mask_channels, self.mask_channels, 3, stride=2, padding=1,
+                               norm_cfg=norm_cfg, conv_cfg=conv_cfg)
+                )
 
         # self.lateral_convs = nn.ModuleList()
         self.combine_convs = nn.ModuleList()
         # self.proto_convs = nn.ModuleList()
         for i in range(num_levels):
+            # convs = nn.ModuleList()
             # feats_stride = 1 if i==0 else 2
             # if self.groups:
             #     self.lateral_convs.append(ConvModule(self.mask_channels,self.mask_channels, 3, stride=feats_stride, padding=1,
@@ -79,27 +91,56 @@ class SemanticPyramidNeck(nn.Module):
         #     conv_cfg=self.conv_cfg,
         #     norm_cfg=self.norm_cfg)
         # self.conv_logits = nn.Conv2d(conv_out_channels, self.num_classes, 1)
-            self.combine_convs.append(
-                nn.Sequential(
-                ConvModule(self.mask_channels+self.feature_channels,
-                           self.conv_out_channels,
-                           3,
-                           padding=1,
-                           norm_cfg=norm_cfg,
-                           conv_cfg=conv_cfg),
-                ConvModule(self.conv_out_channels,
-                           self.conv_out_channels,
-                           3,
-                           padding=1,
-                           norm_cfg=norm_cfg,
-                           conv_cfg=conv_cfg),
-                ConvModule(self.conv_out_channels,
-                           self.conv_out_channels,
-                           3,
-                           padding=1,
-                           norm_cfg=norm_cfg,
-                           conv_cfg=conv_cfg)))
-
+        #     for j in range(self.num_convs):
+        #         in_channel = self.mask_channels + self.feature_channels if j==0 else self.feature_channels
+        #         convs.append(ConvModule(in_channel, self.conv_out_channels, 3, padding=1,
+        #                                 norm_cfg=norm_cfg, conv_cfg=conv_cfg))
+        #     self.combine_convs.append(convs)
+            if self.num_convs == 3:
+                self.combine_convs.append(
+                    nn.Sequential(
+                    ConvModule(self.mask_channels+self.feature_channels,
+                               self.conv_out_channels,
+                               3,
+                               padding=1,
+                               norm_cfg=norm_cfg,
+                               conv_cfg=conv_cfg),
+                    ConvModule(self.conv_out_channels,
+                               self.conv_out_channels,
+                               3,
+                               padding=1,
+                               norm_cfg=norm_cfg,
+                               conv_cfg=conv_cfg),
+                    ConvModule(self.conv_out_channels,
+                               self.conv_out_channels,
+                               3,
+                               padding=1,
+                               norm_cfg=norm_cfg,
+                               conv_cfg=conv_cfg)))
+            elif self.num_convs == 2:
+                self.combine_convs.append(
+                    nn.Sequential(
+                        ConvModule(self.mask_channels + self.feature_channels,
+                                   self.conv_out_channels,
+                                   3,
+                                   padding=1,
+                                   norm_cfg=norm_cfg,
+                                   conv_cfg=conv_cfg),
+                        ConvModule(self.conv_out_channels,
+                                   self.conv_out_channels,
+                                   3,
+                                   padding=1,
+                                   norm_cfg=norm_cfg,
+                                   conv_cfg=conv_cfg)))
+            elif self.num_convs == 1:
+                self.combine_convs.append(
+                    nn.Sequential(
+                        ConvModule(self.mask_channels + self.feature_channels,
+                                   self.conv_out_channels,
+                                   3,
+                                   padding=1,
+                                   norm_cfg=norm_cfg,
+                                   conv_cfg=conv_cfg)))
 
     def init_weights(self):
         pass
@@ -110,8 +151,18 @@ class SemanticPyramidNeck(nn.Module):
         masks = F.interpolate(masks, feats[0].size()[-2:])
         feats = list(feats)
         for i in range(self.num_levels):
-            masks = F.interpolate(masks, feats[i].size()[-2:])
-            # protos = self.proto_convs[i](masks)
+            if not i==0:
+                if self.ds_method == 'interpolate':
+                    masks = F.interpolate(masks, feats[i].size()[-2:])
+                elif self.ds_method == 'max_pool':
+                    masks = F.adaptive_max_pool2d(masks, feats[i].size()[-2:])
+                elif self.ds_method == 'avg_pool':
+                    masks = F.adaptive_avg_pool2d(masks, feats[i].size()[-2:])
+                elif self.ds_method == 'conv':
+                    masks = self.ds_convs[i-1](masks)
+                else:
+                    raise NotImplementedError
+                # protos = self.proto_convs[i](masks)
             feats[i] = self.combine_convs[i](torch.cat([feats[i], masks], dim=1)) + feats[i]
         return tuple(feats)
 
