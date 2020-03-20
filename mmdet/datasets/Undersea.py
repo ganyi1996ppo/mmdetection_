39c532cca2560181fdbff1a82bfe16edd3b07223
Yi G <627780002ganyi@sjtu.edu.cn>
2020-03-20 22:43:13 +0800

    rewrite

--- mmdet/datasets/Undersea.py
+++ mmdet/datasets/Undersea.py
@@ -71,7 +71,7 @@ class UnderseaDataset(CustomDataset):
             else:
                 gt_bboxes.append(bbox)
                 gt_labels.append(self.cat2label[ann['category_id']])
-                gt_masks_ann.append(ann['segmentation'])
+                # gt_masks_ann.append(ann['segmentation'])
 
         if gt_bboxes:
             gt_bboxes = np.array(gt_bboxes, dtype=np.float32)
