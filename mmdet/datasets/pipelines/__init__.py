8bb5882d6fdcaa2a4833b46214ef24f0e018164d
Yi G <627780002ganyi@sjtu.edu.cn>
2020-03-21 13:24:15 +0800

    instaboost

--- mmdet/datasets/pipelines/__init__.py
+++ mmdet/datasets/pipelines/__init__.py
@@ -6,11 +6,12 @@ from .test_aug import MultiScaleFlipAug
 from .transforms import (Expand, MinIoURandomCrop, Normalize, Pad,
                          PhotoMetricDistortion, RandomCrop, RandomFlip, Resize,
                          SegResizeFlipPadRescale)
+from .instaboost import InstaBoost
 
 __all__ = [
     'Compose', 'to_tensor', 'ToTensor', 'ImageToTensor', 'ToDataContainer',
     'Transpose', 'Collect', 'LoadAnnotations', 'LoadImageFromFile',
     'LoadProposals', 'MultiScaleFlipAug', 'Resize', 'RandomFlip', 'Pad',
     'RandomCrop', 'Normalize', 'SegResizeFlipPadRescale', 'MinIoURandomCrop',
-    'Expand', 'PhotoMetricDistortion'
+    'Expand', 'PhotoMetricDistortion', 'InstaBoost'
 ]
