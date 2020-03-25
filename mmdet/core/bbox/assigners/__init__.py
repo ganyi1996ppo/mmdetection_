5eab3bf8dec74f2ce09b80fe18423b9c380a8d34
Yi G <627780002ganyi@sjtu.edu.cn>
2020-03-25 14:03:34 +0800

    undiluted sample

--- mmdet/core/bbox/assigners/__init__.py
+++ mmdet/core/bbox/assigners/__init__.py
@@ -4,8 +4,9 @@ from .base_assigner import BaseAssigner
 from .max_iou_assigner import MaxIoUAssigner
 from .point_assigner import PointAssigner
 from .max_iou_assigner_coeff import MaxIoUAssigner_coeff
+from .max_iou_ud_assigner import MaxIoUUDAssigner
 
 __all__ = [
     'BaseAssigner', 'MaxIoUAssigner', 'ApproxMaxIoUAssigner', 'AssignResult',
-    'PointAssigner', 'MaxIoUAssigner_coeff'
+    'PointAssigner', 'MaxIoUAssigner_coeff', "MaxIoUUDAssigner"
 ]
